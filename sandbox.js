589e7ae437716e758bf8137cb80dbc17e8b76f73
Aditya-Ace <aditya.ace@hotmail.com>
2020-11-23 23:57:14 +0530

    Added the Initial files

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback('could not fetch the data', undefined);
+    }
+  });
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  request.send();
+};
+
+getTodos((err, data) => {
+  if (err) console.log(err);
+  if (data) console.log(data);
+});
